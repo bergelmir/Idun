bb06845d8b7437e5d2dd46cf7ba3966e20653501
Arvid Bergelmir <arvid.bergelmir@flabben.net>
2010-01-19 00:34:23 +0100

    corrected phpdoc indention

--- library/Controller/Exception.php
+++ library/Controller/Exception.php
@@ -1,26 +1,26 @@
 <?php
  
 /**
-* Idun
-*
-* LICENSE
-*
-* This source file is subject to the new BSD license that is bundled with this
-* package in the file LICENSE.txt.
-* If you did not receive a copy of the license and are unable to obtain it
-* through the world-wide-web, please send an email to license@flabben.net so we
-* can send you a copy immediately.
-*
-* @category  Idun
-* @package   Idun_Controller
-* @copyright Copyright (c) 2010 Arvid Bergelmir (http://www.flabben.net/)
-* @version   $Id:$
-*/
+ * Idun
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled with this
+ * package in the file LICENSE.txt.
+ * If you did not receive a copy of the license and are unable to obtain it
+ * through the world-wide-web, please send an email to license@flabben.net so we
+ * can send you a copy immediately.
+ *
+ * @category  Idun
+ * @package   Idun_Controller
+ * @copyright Copyright (c) 2010 Arvid Bergelmir (http://www.flabben.net/)
+ * @version   $Id:$
+ */
 
 /**
-* @category  Idun
-* @package   Idun_Controller
-* @copyright Copyright (c) 2010 Arvid Bergelmir
-* @author    Arvid Bergelmir <arvid.bergelmir@flabben.net>
-*/
+ * @category  Idun
+ * @package   Idun_Controller
+ * @copyright Copyright (c) 2010 Arvid Bergelmir
+ * @author    Arvid Bergelmir <arvid.bergelmir@flabben.net>
+ */
 class Idun_Controller_Exception extends Idun_Exception {}
