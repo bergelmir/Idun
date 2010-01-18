e33a58e39531d6f448c35cd11c24a649843d5684
Arvid Bergelmir <arvid.bergelmir@flabben.net>
2010-01-18 20:41:48 +0100

    added missing exception classes

---
+++ library/Exception.php
@@ -0,0 +1,26 @@
+<?php
+ 
+/**
+* Idun
+*
+* LICENSE
+*
+* This source file is subject to the new BSD license that is bundled with this
+* package in the file LICENSE.txt.
+* If you did not receive a copy of the license and are unable to obtain it
+* through the world-wide-web, please send an email to license@flabben.net so we
+* can send you a copy immediately.
+*
+* @category  Idun
+* @package   Idun
+* @copyright Copyright (c) 2010 Arvid Bergelmir (http://www.flabben.net/)
+* @version   $Id:$
+*/
+
+/**
+* @category  Idun
+* @package   Idun
+* @copyright Copyright (c) 2010 Arvid Bergelmir
+* @author    Arvid Bergelmir <arvid.bergelmir@flabben.net>
+*/
+class Idun_Exception extends Zend_Exception {}
